abfa90893b31dbe1286ed7dc3fb7d38c9a71339a
John Slee <john.slee@siteminder.com>
2021-06-02 12:59:17 +1000

    optionally spawn a Docker container instead
    Much nicer than typing out the whole Docker commandline yourself with
    all the environment passthru.

--- README.md
+++ README.md
@@ -4,15 +4,23 @@ Yet Another Assume-Role in GOlang
 
 * small, single-file, easily-audited source code
 * optionally spawns a `tmux` server
+* optionally spawns a Docker container (default: `alpine`)
 * uses existing AWS SDK config (`$HOME/.aws/credentials` profiles)
 * prompts for MFA if required
 
 ## usage
 
 ```
+$ ./yaargo --help
 Usage of ./yaargo:
+  -docker
+    	pass role credentials to a new Docker container
   -duration duration
     	override credential lifetime (default 1h0m0s)
+  -entrypoint string
+    	Docker entrypoint to use (set to empty string to use image default) (default "/bin/sh")
+  -image string
+    	Docker image to use (default "alpine")
   -profile string
     	AWS profile name (from $HOME/.aws/credentials)
   -tmux
--- main.go
+++ main.go
@@ -13,9 +13,12 @@ import (
 )
 
 type config struct {
-	duration time.Duration
-	profile  string
-	tmux     bool
+	duration   time.Duration
+	profile    string
+	tmux       bool
+	docker     bool
+	image      string
+	entrypoint string
 }
 
 func configureFromFlags() config {
@@ -23,8 +26,63 @@ func configureFromFlags() config {
 	flag.DurationVar(&c.duration, "duration", time.Hour, "override credential lifetime")
 	flag.StringVar(&c.profile, "profile", "", "AWS profile name (from $HOME/.aws/credentials)")
 	flag.BoolVar(&c.tmux, "tmux", false, "invoke tmux instead of $SHELL")
+	flag.BoolVar(&c.docker, "docker", false, "pass role credentials to a new Docker container")
+	flag.StringVar(&c.image, "image", "alpine", "Docker image to use")
+	flag.StringVar(&c.entrypoint, "entrypoint", "/bin/sh", "Docker entrypoint to use (set to empty string to use image default)")
 	flag.Parse()
+	if c.docker && c.tmux {
+		log.Fatal("Aborting: specify exactly one of -tmux or -docker, please")
+	}
 	return c
+}
+
+func (c config) tmuxCommand(expiresAt time.Time) *exec.Cmd {
+	tmuxsocket := fmt.Sprintf("%s-%d", c.profile, expiresAt.Unix())
+	// we start with a dedicated socket, unfortunately, because otherwise the
+	// environment variables may cross-pollinate into other tmux sessions via
+	// inheritance from the tmux server :-(
+	args := []string{"-L", tmuxsocket, "new-session", "-s", tmuxsocket}
+	return exec.Command("tmux", args...)
+}
+
+func (c config) shellCommand() *exec.Cmd {
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "/bin/sh" // lol windows
+	}
+	return exec.Command(shell)
+}
+
+func (c config) dockerCommand() *exec.Cmd {
+	args := []string{
+		"run",
+		"--interactive",
+		"--tty",
+		"--rm",
+		"--env", "ARG_ROLE_CREDS_EXPIRE_AT",
+		"--env", "AWS_ACCESS_KEY_ID",
+		"--env", "AWS_PROFILE",
+		"--env", "AWS_REGION",
+		"--env", "AWS_SECRET_ACCESS_KEY",
+		"--env", "AWS_SESSION_TOKEN",
+	}
+	if c.entrypoint != "" {
+		args = append(args, "--entrypoint")
+		args = append(args, c.entrypoint)
+	}
+	args = append(args, c.image)
+	return exec.Command("docker", args...)
+}
+
+func (c config) command(expiresAt time.Time) *exec.Cmd {
+	switch {
+	case c.docker:
+		return c.dockerCommand()
+	case c.tmux:
+		return c.tmuxCommand(expiresAt)
+	default:
+		return c.shellCommand()
+	}
 }
 
 func main() {
@@ -51,21 +109,7 @@ func main() {
 	}
 	os.Setenv("ARG_ROLE_CREDS_EXPIRE_AT", fmt.Sprint(expiresAt.Unix()))
 	os.Setenv("ARG_PROFILE", cfg.profile)
-	var cmd *exec.Cmd
-	if cfg.tmux {
-		tmuxsocket := fmt.Sprintf("%s-%d", cfg.profile, expiresAt.Unix())
-		// we start with a dedicated socket, unfortunately, because otherwise the
-		// environment variables may cross-pollinate into other tmux sessions via
-		// inheritance from the tmux server :-(
-		args := []string{"-L", tmuxsocket, "new-session", "-s", tmuxsocket}
-		cmd = exec.Command("tmux", args...)
-	} else {
-		shell := os.Getenv("SHELL")
-		if shell == "" {
-			shell = "/bin/sh" // lol windows
-		}
-		cmd = exec.Command(shell)
-	}
+	cmd := cfg.command(expiresAt)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
